fba21799019a830cbb9d6141adc8beb797fd7db9
smitnk <sumitnaikna29@gmail.com>
2026-09-26 11:25:16 +0530

    Add native GP stroke translation primitive

--- native/blender_gp/project_grease_gp_backend.h
+++ native/blender_gp/project_grease_gp_backend.h
@@ -47,6 +47,7 @@ class Backend {
   bool delete_stroke(int index);
   bool delete_last_stroke();
   bool duplicate_stroke(int index);
+  bool translate_stroke(int index, float dx, float dy, float dz);
 
   // Writes native GP stroke points. No Android Canvas rendering is used.
   bool begin_stroke(const StrokeStyle& style);

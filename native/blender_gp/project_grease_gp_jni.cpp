ff008aef68d1a46ad68a854e1d02d20d042ec8b8
smitnk <sumitnaikna29@gmail.com>
2026-09-26 20:48:03 +0530

    Align JNI symbols with Android package

--- native/blender_gp/project_grease_gp_jni.cpp
+++ native/blender_gp/project_grease_gp_jni.cpp
@@ -21,21 +21,21 @@ jlong to_handle(ProjectGreaseGPHandle *handle)
 }  // namespace
 
 extern "C" JNIEXPORT jlong JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeCreate(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeCreate(
     JNIEnv *, jclass)
 {
   return to_handle(project_grease_gp_create());
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeDestroy(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeDestroy(
     JNIEnv *, jclass, jlong handle)
 {
   project_grease_gp_destroy(from_handle(handle));
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeBeginStroke(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeBeginStroke(
     JNIEnv *, jclass, jlong handle, jint material_index, jfloat thickness)
 {
   return project_grease_gp_begin_stroke(
@@ -43,7 +43,7 @@ Java_com_projectgrease_nativebridge_GPNative_nativeBeginStroke(
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeAddPoint(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeAddPoint(
     JNIEnv *,
     jclass,
     jlong handle,
@@ -60,28 +60,28 @@ Java_com_projectgrease_nativebridge_GPNative_nativeAddPoint(
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeEndStroke(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeEndStroke(
     JNIEnv *, jclass, jlong handle)
 {
   return project_grease_gp_end_stroke(from_handle(handle)) != 0;
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeRender(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeRender(
     JNIEnv *, jclass, jlong handle)
 {
   return project_grease_gp_render(from_handle(handle)) != 0;
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativeStrokeCount(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativeStrokeCount(
     JNIEnv *, jclass, jlong handle)
 {
   return project_grease_gp_stroke_count(from_handle(handle));
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_com_projectgrease_nativebridge_GPNative_nativePointCount(
+Java_com_smitnk_projectgrease_nativebridge_GPNative_nativePointCount(
     JNIEnv *, jclass, jlong handle)
 {
   return project_grease_gp_point_count(from_handle(handle));

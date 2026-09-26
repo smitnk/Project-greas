d735fafd7a2a2d3281dfb1a829fbe6f2e6da3f6a
smitnk <sumitnaikna29@gmail.com>
2026-09-26 19:50:14 +0530

    Start Android native GP bridge: project_grease_gp_bridge.h

---
+++ native/blender_gp/project_grease_gp_bridge.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct ProjectGreaseGPHandle ProjectGreaseGPHandle;
+
+typedef struct ProjectGreaseGPPoint {
+  float x;
+  float y;
+  float z;
+  float pressure;
+  float strength;
+  float time;
+} ProjectGreaseGPPoint;
+
+typedef struct ProjectGreaseGPStrokeStyle {
+  int material_index;
+  float thickness;
+} ProjectGreaseGPStrokeStyle;
+
+ProjectGreaseGPHandle *project_grease_gp_create(void);
+void project_grease_gp_destroy(ProjectGreaseGPHandle *handle);
+
+int project_grease_gp_begin_stroke(
+    ProjectGreaseGPHandle *handle,
+    int material_index,
+    float thickness);
+
+int project_grease_gp_add_point(
+    ProjectGreaseGPHandle *handle,
+    ProjectGreaseGPPoint point);
+
+int project_grease_gp_end_stroke(ProjectGreaseGPHandle *handle);
+int project_grease_gp_render(ProjectGreaseGPHandle *handle);
+
+int project_grease_gp_stroke_count(const ProjectGreaseGPHandle *handle);
+int project_grease_gp_point_count(const ProjectGreaseGPHandle *handle);
+
+const char *project_grease_gp_last_error(
+    const ProjectGreaseGPHandle *handle);
+
+#ifdef __cplusplus
+}
+#endif
